8d4a985dfc0a8fc0e53d9c81e77d9f238550ef10
Andres_p11 <andres.ponce.de.leon.huerta@gmail.com>
2017-01-23 11:56:43 -0600

    AgregarClase

---
+++ Galileo/Email.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace Galileo
+{
+    public class Email
+    {
+
+        public static void Correo(string Remitente,int ID)
+        {
+
+
+
+        }
+    
+    }
+
+
+
+}
